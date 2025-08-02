899057333110f18770f8b7a56deac7a631321572
Renat Husnetdinov <renatilshatovich@yandex.ru>
2025-08-02 22:08:06 +0300

    Implement TakeDamage for enemies

--- Assets/RaycastExamples/RaycastExample/Scripts/Enemy.cs
+++ Assets/RaycastExamples/RaycastExample/Scripts/Enemy.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : MonoBehaviour, IDamageable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private const string TakeDamageTriggerKey = "TakeDamage";
+    private const string IsDeadKey = "IsDead";
+    
+    [SerializeField] private Animator _animator;
+    [SerializeField] private Collider _bodyCollider;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private int _maxHealth;
+    private int _currentHealth;
+
+    private void Awake() => 
+        _currentHealth = _maxHealth;
+
+    public void TakeDamage(int damege)
     {
+        if (damege < 0)
+        {
+            Debug.LogError(damege);
+            return;
+        }
+
+        _currentHealth -= damege;
+
+        if (_currentHealth < 0)
+        {
+            _currentHealth = 0;
+            _bodyCollider.enabled = false;
+            _animator.SetBool(IsDeadKey, true);
+            return;
+        }
         
+        _animator.SetTrigger(TakeDamageTriggerKey);
     }
 }
